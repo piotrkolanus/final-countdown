038ee6524ed6521a295c3f30acb2ddad85eecf92
piotrkolanus <piotr.kolanus@gmail.com>
2019-06-06 08:39:32 +0200

    added useEffect

--- .cache/api-runner-browser-plugins.js
+++ .cache/api-runner-browser-plugins.js
@@ -1,4 +1,1 @@
-module.exports = [{
-      plugin: require('C:/Users/Piotr/final-countdown/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
+module.exports = []
--- src/components/timer.js
+++ src/components/timer.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 
 const Timer = () => {
   const [{ days, hours, minutes, seconds }, setTimeUnit] = useState({
@@ -10,23 +10,29 @@ const Timer = () => {
   const [isCountdownEnd, setIsCountdownEnd] = useState(false)
   const finalDate = new Date("Jun 24, 2019 09:00:00").getTime()
 
-  const timer = setInterval(function() {
-    let now = new Date().getTime()
+  useEffect(() => {
+    const timer = setInterval(function() {
+      let now = new Date().getTime()
 
-    let interval = finalDate - now
+      let interval = finalDate - now
 
-    if (interval === 0) {
-      setIsCountdownEnd(true)
-      this.clearInterval(timer)
+      if (interval === 0) {
+        setIsCountdownEnd(true)
+        this.clearInterval(timer)
+      }
+
+      let d = Math.floor(interval / (1000 * 60 * 60 * 24))
+      let h = Math.floor((interval % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+      let m = Math.floor((interval % (1000 * 60 * 60)) / (1000 * 60))
+      let s = Math.floor((interval % (1000 * 60)) / 1000)
+
+      setTimeUnit({ days: d, hours: h, minutes: m, seconds: s })
+    }, 1000)
+
+    return () => {
+      clearInterval(timer)
     }
-
-    let d = Math.floor(interval / (1000 * 60 * 60 * 24))
-    let h = Math.floor((interval % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-    let m = Math.floor((interval % (1000 * 60 * 60)) / (1000 * 60))
-    let s = Math.floor((interval % (1000 * 60)) / 1000)
-
-    setTimeUnit({ days: d, hours: h, minutes: m, seconds: s })
-  }, 1000)
+  }, [])
 
   return !isCountdownEnd ? (
     <div>
